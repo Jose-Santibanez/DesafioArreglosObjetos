7e87bf1c528116a4a1bf1ecd537c227981edd73a
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-02-08 19:28:00 -0300

    terminando el desafio, Faltaria crear el render

--- assets/js/propiedades_alquiler.js
+++ assets/js/propiedades_alquiler.js
@@ -1,3 +1,4 @@
+
 const propiedades_alquiler = [
     {    
            nombre: 'Hare Nua',
@@ -42,15 +43,197 @@ const propiedades_alquiler = [
             costo: 42000,
             smoke: true,
             pets: true
+},
+{    
+       nombre: 'Hare Nua',
+       src: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/00/97/23/hare-nua.jpg?w=1200&h=-1&s=1',
+       descripcion: 'Un oasis de calma y naturaleza ubicado en pleno centro de Rapa Nui.',
+       ubicacion: 'Atamu Tekena, Hanga Roa, Isla de Pascua 2770000 Chile',
+       habitaciones: 4,
+       banos: 2,
+       costo: 20000,
+       smoke: true,
+       pets: true
+},
+{    
+        nombre: 'Hotel Bidasoa',
+        src: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/f0/ca/ab/caption.jpg?w=1100&h=-1&s=1',
+        descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: 'Hotel Bidasoa está situado frente al Museo de la Moda, en el barrio exclusivo de Vitacura',
+        habitaciones: 4,
+        banos: 2,
+        costo: 25000,
+        smoke: false,
+        pets: true
+},
+{    
+        nombre: 'Solar Madariaga Hotel',
+        src: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2d/f4/4a/19/caption.jpg?w=1200&h=-1&s=1',
+        descripcion: 'Solar Madariaga Hotel ha sido construido en una hermosa propiedad, la casona post-colonial de nuestra familia.',
+        ubicacion: 'Gabriela Mistral 683, Vicuña, Vicuña 1760762 Chile',
+        habitaciones: 3,
+        banos:2,
+        costo: 45000,
+        smoke: true,
+        pets: true
+},
+{    
+        nombre: 'Casa Primal',
+        src: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/84/f1/37/caption.jpg?w=800&h=-1&s=1',
+        descripcion: 'ofrece alojamiento con piscina al aire libre, parking privado gratis, jardín y salón de uso común.',
+        ubicacion: 'Camino al Volcán 30517, San Alfonso, San José de Maipo 9460000 Chile',
+        habitaciones: 3,
+        banos: 2,
+        costo: 42000,
+        smoke: true,
+        pets: true
 }
 ]
-const etiquetaCard = document.querySelector(".alquiler");
-let contador = 0;
-let template =" ";v
-for (let propiedad of propiedades_alquiler){
-       if(contador > 2 ){
-              template += `
-              
-              `
+const contenedorAlquilerHtml = document.querySelector(".contenedorAlquilerhtml")
+let templateCompletohtml =" ";
+       for(let propiedad of propiedades_alquiler){
+              templateCompletohtml += `
+              <div class="col-md-4 mb-4">
+                     <div class="card">
+                            <img
+                            src="${propiedad.src}"
+                            class="card-img-top"
+                            alt="Imagen del departamento"
+                            />
+                            <div class="card-body">
+                                   <h5 class="card-title">
+                                   ${propiedad.nombre}
+                                   </h5>
+                                   <p class="card-text">
+                                   ${propiedad.descripcion}
+                                   de la ciudad, cerca de todo.
+                                   </p>
+                                   <p>
+                                   <i class="fas fa-map-marker-alt"></i>${propiedad.ubicacion} 
+                                   </p>
+                                   <p>
+                                   <i class="fas fa-bed"></i> ${propiedad.habitaciones} |
+                                   <i class="fas fa-bath"></i> ${propiedad.banos}
+                                   </p>
+                                   <p><i class="fas fa-dollar-sign"></i>${propiedad.costo}</p>`
+                                                 if(propiedad.smoke === true && propiedad.pets === true){
+                                                     let itemFumar = `<p class="text-success"><i class="fas fa-smoking"></i>Se permite Fumar</p>`
+                                                     templateCompletohtml += itemFumar;
+                                                     let itemMascota = `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>`
+                                                     templateCompletohtml+=itemMascota
+                                                     }
+                                                 else{
+                                                     let itemFumar = `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite Fumar</p>`
+                                                     templateCompletohtml += itemFumar
+                                                     let itemMascota = `<p class="text-danger"><i class="fa-solid fa-ban"></i> Mascotas permitidas</p>`
+                                                     templateCompletohtml+=itemMascota
+                                                     }                                                             
+templateCompletohtml +=  `</div>
+                                   </div>
+                                          </div>`;
        }
-}
+contenedorAlquilerHtml.innerHTML = templateCompletohtml;
+
+
+const cardAlquiler = document.querySelector(".contenedorAlquiler");
+let iterador = 0;
+let templatecard =" ";
+       for (let propiedad of propiedades_alquiler){
+              if(iterador <= 2 ){
+                     templatecard += `
+                     <div class="col-md-4 mb-4">
+                            <div class="card">
+                                   <img
+                                   src="${propiedad.src}"
+                                   class="card-img-top"
+                                   alt="Imagen del departamento"
+                                   />
+                                   <div class="card-body">
+                                          <h5 class="card-title">
+                                          ${propiedad.nombre}
+                                          </h5>
+                                          <p class="card-text">
+                                          ${propiedad.descripcion}
+                                          de la ciudad, cerca de todo.
+                                          </p>
+                                          <p>
+                                          <i class="fas fa-map-marker-alt"></i>${propiedad.ubicacion} 
+                                          </p>
+                                          <p>
+                                          <i class="fas fa-bed"></i> ${propiedad.habitaciones} |
+                                          <i class="fas fa-bath"></i> ${propiedad.banos}
+                                          </p>
+                                          <p><i class="fas fa-dollar-sign"></i>${propiedad.costo}</p>`
+                                                        if(propiedad.smoke === true && propiedad.pets === true){
+                                                            let itemFumar = `<p class="text-success"><i class="fas fa-smoking"></i>Se permite Fumar</p>`
+                                                            templatecard += itemFumar;
+                                                            let itemMascota = `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>`
+                                                            templatecard+=itemMascota
+                                                            }
+                                                        else{
+                                                            let itemFumar = `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite Fumar</p>`
+                                                            templatecard += itemFumar
+                                                            let itemMascota = `<p class="text-danger"><i class="fa-solid fa-ban"></i> Mascotas permitidas</p>`
+                                                            templatecard+=itemMascota
+                                                            }                                                             
+                   templatecard +=  `</div>
+                                          </div>
+                                                 </div>`;
+              }
+              iterador++;
+       }
+cardAlquiler.innerHTML += templatecard// Aqui va el innerhtml
+
+// Agregar evento para mostrar todas las CARDS
+
+const btnAlquiler = document.querySelector(".btn-alquiler"); // Obtengo el elemento del DOM btn
+
+btnAlquiler.addEventListener("click", ()=>{
+       let templateCompleto =" ";
+       for(let propiedad of propiedades_alquiler){
+              templateCompleto += `
+              <div class="col-md-4 mb-4">
+                     <div class="card">
+                            <img
+                            src="${propiedad.src}"
+                            class="card-img-top"
+                            alt="Imagen del departamento"
+                            />
+                            <div class="card-body">
+                                   <h5 class="card-title">
+                                   ${propiedad.nombre}
+                                   </h5>
+                                   <p class="card-text">
+                                   ${propiedad.descripcion}
+                                   de la ciudad, cerca de todo.
+                                   </p>
+                                   <p>
+                                   <i class="fas fa-map-marker-alt"></i>${propiedad.ubicacion} 
+                                   </p>
+                                   <p>
+                                   <i class="fas fa-bed"></i> ${propiedad.habitaciones} |
+                                   <i class="fas fa-bath"></i> ${propiedad.banos}
+                                   </p>
+                                   <p><i class="fas fa-dollar-sign"></i>${propiedad.costo}</p>`
+                                                 if(propiedad.smoke === true && propiedad.pets === true){
+                                                     let itemFumar = `<p class="text-success"><i class="fas fa-smoking"></i>Se permite Fumar</p>`
+                                                     templateCompleto += itemFumar;
+                                                     let itemMascota = `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>`
+                                                     templateCompleto+=itemMascota
+                                                     }
+                                                 else{
+                                                     let itemFumar = `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite Fumar</p>`
+                                                     templateCompleto += itemFumar
+                                                     let itemMascota = `<p class="text-danger"><i class="fa-solid fa-ban"></i> Mascotas permitidas</p>`
+                                                     templateCompleto+=itemMascota
+                                                     }                                                             
+       templateCompleto +=  `</div>
+                                   </div>
+                                          </div>`;
+       }
+       cardAlquiler.innerHTML += templateCompleto// Aqui va el innerhtml
+})
+
+
+
+
--- assets/js/propiedades_ventas.js
+++ assets/js/propiedades_ventas.js
@@ -24,7 +24,7 @@ const propiedades_ventas = [
            habitaciones: 4,
            banos:4,
            costo: 170000000,
-           smoke: true,
+           smoke: true, 
            pets: false
     },
     {    
@@ -59,16 +59,106 @@ const propiedades_ventas = [
             costo: 100220000,
             smoke: false,
             pets: false
+},
+{    
+    nombre: 'Apartamento de lujo en zona exclusiva',
+    src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1278280691588941147/original/b3766aa6-fc4b-4761-8636-13175a988368.jpeg?im_w=1200&im_format=avif',
+    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    habitaciones: 4,
+    banos:4,
+    costo: 170000000,
+    smoke: true, 
+    pets: false
+},
+{    
+     nombre: 'Apartamento acogedor en la montaña',
+     src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+     descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+     ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
+     habitaciones: 2,
+     banos: 1,
+     costo: 90000000,
+     smoke: true,
+     pets: true
+},
+{    
+     nombre: 'Penthouse de lujo con terraza panorámica',
+     src:"https://a0.muscache.com/im/pictures/miso/Hosting-37399805/original/44a1d6c5-edf2-4143-b567-b16ea1f52523.jpeg?im_w=1440&im_format=avif",
+     descripcion: '567 Skyline Avenue, Skyview City, CA 56789',
+     ubicacion: '',
+     habitaciones: 3,
+     banos:3,
+     costo: 450000000,
+     smoke: false,
+     pets: true
+},
+{    
+     nombre: 'Condominio moderno en zona residencial',
+     src: 'https://a0.muscache.com/im/pictures/9f5ac2de-c98a-4c0e-b1c8-9599eb813086.jpg?im_w=1200&im_format=avif',
+     descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+     ubicacion: '123 Main Street, Anytown, CA 91234',
+     habitaciones: 2,
+     banos: 2,
+     costo: 100220000,
+     smoke: false,
+     pets: false
 }
 ]
+const contenedorVentasHtml = document.querySelector(".contenedorVentasHtml");
+let templateCompletoVentasHtml =" ";
+       for(let propiedad of propiedades_ventas){
+        templateCompletoVentasHtml += `
+              <div class="col-md-4 mb-4">
+                     <div class="card">
+                            <img
+                            src="${propiedad.src}"
+                            class="card-img-top"
+                            alt="Imagen del departamento"
+                            />
+                            <div class="card-body">
+                                   <h5 class="card-title">
+                                   ${propiedad.nombre}
+                                   </h5>
+                                   <p class="card-text">
+                                   ${propiedad.descripcion}
+                                   de la ciudad, cerca de todo.
+                                   </p>
+                                   <p>
+                                   <i class="fas fa-map-marker-alt"></i>${propiedad.ubicacion} 
+                                   </p>
+                                   <p>
+                                   <i class="fas fa-bed"></i> ${propiedad.habitaciones} |
+                                   <i class="fas fa-bath"></i> ${propiedad.banos}
+                                   </p>
+                                   <p><i class="fas fa-dollar-sign"></i>${propiedad.costo}</p>`
+                                                 if(propiedad.smoke === true && propiedad.pets === true){
+                                                     let itemFumar = `<p class="text-success"><i class="fas fa-smoking"></i>Se permite Fumar</p>`
+                                                     templateCompletoVentasHtml += itemFumar;
+                                                     let itemMascota = `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>`
+                                                     templateCompletoVentasHtml+=itemMascota
+                                                     }
+                                                 else{
+                                                     let itemFumar = `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite Fumar</p>`
+                                                     templateCompletoVentasHtml += itemFumar
+                                                     let itemMascota = `<p class="text-danger"><i class="fa-solid fa-ban"></i> Mascotas permitidas</p>`
+                                                     templateCompletoVentasHtml+=itemMascota
+                                                     }                                                             
+  templateCompletoVentasHtml +=  `</div>
+                                   </div>
+                                          </div>`;
+       }
+       contenedorVentasHtml.innerHTML = templateCompletoVentasHtml;
+
 // obtenemos el contendor de las card donde se insertara el template
 const etiquetaCard = document.querySelector(".ventas");
 
-// Ahora vamos a recorrer el arreglo
-
+// Variable acumuladora
 let template = '';   // Declaramos el template fuera para evitar re-rederizado y en let por que se actualiza la información del mismo
-
+// Contador para contar las iteraciones del for y deterno con una condición.         
 let contador = 0;
+
+// Ahora vamos a recorrer el arreglo
 for (let propiedad of propiedades_ventas){
     
     if(contador <= 2){
@@ -109,5 +199,53 @@ for (let propiedad of propiedades_ventas){
          contador++                                                               
         }etiquetaCard.innerHTML += template;
         
-            
+   // Agregar evento para mostrar todas las CARDS
+
+const btnVenta = document.querySelector(".btn-ventas"); // Obtengo el elemento del DOM btn
+    
+    btnVenta.addEventListener("click", ()=>{
+        let templateCompleto =" ";
+        for(let propiedad of propiedades_ventas){
+               templateCompleto += `
+               <div class="col-md-4 mb-4">
+                      <div class="card">
+                             <img
+                             src="${propiedad.src}"
+                             class="card-img-top"
+                             alt="Imagen del departamento"
+                             />
+                             <div class="card-body">
+                                    <h5 class="card-title">
+                                    ${propiedad.nombre}
+                                    </h5>
+                                    <p class="card-text">
+                                    ${propiedad.descripcion}
+                                    de la ciudad, cerca de todo.
+                                    </p>
+                                    <p>
+                                    <i class="fas fa-map-marker-alt"></i>${propiedad.ubicacion} 
+                                    </p>
+                                    <p>
+                                    <i class="fas fa-bed"></i> ${propiedad.habitaciones} |
+                                    <i class="fas fa-bath"></i> ${propiedad.banos}
+                                    </p>
+                                    <p><i class="fas fa-dollar-sign"></i>${propiedad.costo}</p>`
+                                                  if(propiedad.smoke === true && propiedad.pets === true){
+                                                      let itemFumar = `<p class="text-success"><i class="fas fa-smoking"></i>Se permite Fumar</p>`
+                                                      templateCompleto += itemFumar;
+                                                      let itemMascota = `<p class="text-success"><i class="fas fa-paw"></i> Mascotas permitidas</p>`
+                                                      templateCompleto+=itemMascota
+                                                      }
+                                                  else{
+                                                      let itemFumar = `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite Fumar</p>`
+                                                      templateCompleto += itemFumar
+                                                      let itemMascota = `<p class="text-danger"><i class="fa-solid fa-ban"></i> Mascotas permitidas</p>`
+                                                      templateCompleto+=itemMascota
+                                                      }                                                             
+        templateCompleto +=  `</div>
+                                    </div>
+                                           </div>`;
+        }
+        etiquetaCard.innerHTML += templateCompleto// Aqui va el innerhtml
+ })   
               
